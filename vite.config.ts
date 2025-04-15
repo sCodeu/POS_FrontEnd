6efc05b1b45c9a36c44cc4cfb857a46d39ce161e
sCodeu <sachindu99u@gmail.com>
2025-04-15 19:04:39 +0530

    Set Up Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/POS_FrontEnd/",
+  server: {
+    open: true,
+    port: 3001,
+  }
 })
